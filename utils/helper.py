670f30b4ea98e98386f69651b120873bffc9692b
meetthehorizon <kshitij.sharma.cse22@itbhu.ac.in>
2024-01-24 03:26:37 +0530

    small changes

--- utils/helper.py
+++ utils/helper.py
@@ -1,14 +1,21 @@
-import torch 
+import torch
+
+
 def checkpoint(model, filename):
-	torch.save(model.state_dict(), filename)
+    """Save model state dict to file."""
+    torch.save(model.state_dict(), filename)
+
 
 def resume(model, filename):
-	model.load_state_dict(torch.load(filename))
+    """Load model state dict from file."""
+    model.load_state_dict(torch.load(filename))
+
 
 def print_progress(epoch, batches_trained, total_batches, bar_length=20):
+    """Print progress bar."""
     progress = batches_trained / total_batches
     percent = int(progress * 100)
     num_blocks = int(bar_length * progress)
     bar = "=" * num_blocks + "-" * (bar_length - num_blocks)
     progress_str = f"Epoch {epoch} - Batches: {batches_trained}/{total_batches} - {percent}% [{bar}]"
-    print(progress_str, end='\r')
+    print(progress_str, end="\r")
--- utils/pytorch_utils.py
+++ utils/pytorch_utils.py
@@ -5,46 +5,54 @@ from torch.utils.data import Dataset
 from torchvision import transforms
 from PIL import Image
 
+
 class LungDataset(Dataset):
-	def __init__(self, path='data/train', transform=None, num=None):
-		"""Lung segmentation dataset.
-		
-		Parameters
-		----------
-		image_path : str
-			Path to images directory
-		mask_path : str
-			Path to masks directory
-		transform : callable
-			Transformation to apply to image and mask
-		"""
-		image_dir = path + '/image/'
-		mask_dir = path + '/mask/'
-		self.image_paths = [image_dir + f for f in os.listdir(image_dir) if f.endswith('.png')]
-		self.mask_paths = [mask_dir + f for f in os.listdir(mask_dir) if f.endswith('.jpeg')]
+    def __init__(self, path="data/train", transform=None, num=None):
+        """Lung segmentation dataset.
 
-		if num is not None:
-			self.image_paths = self.image_paths[:num]
-			self.mask_paths = self.mask_paths[:num]
+        Parameters
+        ----------
+        image_path : str
+                Path to images directory
+        mask_path : str
+                Path to masks directory
+        transform : callable
+                Transformation to apply to image and mask
+        """
+        image_dir = path + "/image/"
+        mask_dir = path + "/mask/"
+        self.image_paths = [
+            image_dir + f for f in os.listdir(image_dir) if f.endswith(".png")
+        ]
+        self.mask_paths = [
+            mask_dir + f for f in os.listdir(mask_dir) if f.endswith(".jpeg")
+        ]
 
-		self.transform = transform
+        if num is not None:
+            self.image_paths = self.image_paths[:num]
+            self.mask_paths = self.mask_paths[:num]
 
-	def __getitem__(self, index):
-		"""Load image and mask at index."""
-		# print(self.image_paths)
-		if index >= len(self) or index < 0:
-			raise IndexError('Index out of range')
+        self.transform = transform
 
-		image, mask = Image.open(self.image_paths[index]).convert('L'), Image.open(self.mask_paths[index]).convert('L')
+    def __getitem__(self, index):
+        """Load image and mask at index."""
+        # print(self.image_paths)
+        if index >= len(self) or index < 0:
+            raise IndexError("Index out of range")
 
-		if self.transform:
-			image, mask = self.transform(image), self.transform(mask)
-		
-		return image, mask
+        image, mask = Image.open(self.image_paths[index]).convert("L"), Image.open(
+            self.mask_paths[index]
+        ).convert("L")
 
-	def __len__(self):
-		"""Return length of dataset."""
-		return len(self.image_paths)
-	
-if __name__ == '__main__':
-	print('passed')
+        if self.transform:
+            image, mask = self.transform(image), self.transform(mask)
+
+        return image, mask
+
+    def __len__(self):
+        """Return length of dataset."""
+        return len(self.image_paths)
+
+
+if __name__ == "__main__":
+    print("passed")
